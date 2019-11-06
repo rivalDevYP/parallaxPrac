7ae9f914bf73720da003903d5cd35abe1d4c2a07
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-11-06 17:51:55 -0500

    push to heroku

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$(window).scroll(()=>{
-  console.log("hi");
-})
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(5000,()=>{
+  console.log("listening on port 5000");
+});
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'));
+});
